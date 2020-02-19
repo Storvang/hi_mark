603f62f0c45193378a5336a1612c463250d8a00a
sockmaster27 <holg0374@edu.aarhustech.dk>
2020-02-19 15:02:43 +0100

    lmao

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+
+
+scroll_y = 0
+
+
+class Player:
+    def __init__(self, pos, weapon):
+        pass
+
+    def update(self, delta_time):
+        pass
+
+
+class Platform:
+    def __init__(self, y_pos, length):
+        pass
+
+    def update(self, delta_time):
+        pass
+
+
+Mark = Player((0, 0), 0)
+
+quit_game = False
+while not quit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            True
+
+    
+    Mark.update #lmao
